ff3cb44508c5e9ebc14857a7118cd1d5cceff208
AmandaLimasiva <amandalimasiva.com>
2024-04-03 22:44:17 -0300

    Atualizando projeto

---
+++ script.js
@@ -0,0 +1,31 @@
+//Direito Civil
+
+function exibirDialogDC() {
+    document.getElementById('dialog-dc').style.display = 'block'; // Exibe o modal de Direito Civil
+  }
+
+  function fecharDialogDC() {
+    document.getElementById('dialog-dc').style.display = 'none'; // Oculta o modal
+  }
+
+
+  //Direito Digital
+  function exibirDialogDD() {
+    document.getElementById('dialog-dd').style.display = 'block'; // Exibe o modal
+  }
+
+
+function fecharDialogDD() {
+    document.getElementById('dialog-dd').style.display = 'none'; // Oculta o modal
+  }
+
+  //Direito do Consumidor
+  
+  function exibirDialogConsumidor() {
+    document.getElementById('dialogConsumidor').style.display = 'block'; // Exibe o modal
+  }
+
+
+function fecharDialogConsumidor() {
+    document.getElementById('dialogConsumidor').style.display = 'none'; // Oculta o modal
+  } 
